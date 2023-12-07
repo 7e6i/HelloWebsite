d938e36b810684d2c1de1c144c797c3971df07da
Spencer Anderson <sra6342@rit.edu>
2023-12-07 12:34:19 -0500

    its kinda a website now

---
+++ functions.js
@@ -0,0 +1,15 @@
+
+function updateDateTime() {
+  const now = new Date();
+  const currentDateTime = now.toLocaleString();
+  document.querySelector('#datetime').textContent = currentDateTime;
+}
+setInterval(updateDateTime, 100);
+
+
+function generateRandom() {
+  const n = Math.floor(Math.random() * 1000)+1;
+
+  document.querySelector('#random').textContent = n;
+}
+setInterval(generateRandom, 500);
