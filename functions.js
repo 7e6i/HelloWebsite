ae397b805dfb9ce1277a40e6083060ac0e90e1bf
Spencer <spencera.rochester@gmail.com>
2023-12-16 12:22:11 -0500

    crazy

--- functions.js
+++ functions.js
@@ -43,3 +43,22 @@ function age(){
 }
 age();
 
+
+
+function color(){
+
+    const rows = 10;
+    const cols = 60;
+
+    var x = Math.floor(Math.random()*rows).toString();
+    var y = Math.floor(Math.random()*cols).toString();
+
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    var element = document.getElementById("("+x+","+y+")");
+    if (element){
+        element.style.backgroundColor = "#"+randomColor;
+    }
+
+}
+
+setInterval(color, 1);
