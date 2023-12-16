c25b258fb364631956833418376d4ff0639591a2
Spencer <spencera.rochester@gmail.com>
2023-12-15 21:12:04 -0500

    upgrades

--- functions.js
+++ functions.js
@@ -2,16 +2,42 @@
 function updateDateTime() {
   const now = new Date();
   const currentDateTime = now.toLocaleString();
-  document.querySelector('#datetime').textContent = currentDateTime;
+
+
+  var element = document.querySelector('#datetime');
+  if (element){
+    element.textContent = currentDateTime;
+  }
+
 }
 updateDateTime();
 setInterval(updateDateTime, 100);
 
 
+
+
 function generateRandom() {
   const n = Math.floor(Math.random() * 1000)+1;
 
-  document.querySelector('#random').textContent = n;
+  var element = document.querySelector('#random');
+  if (element){
+    element.textContent = n;
+  }
 }
 generateRandom();
 setInterval(generateRandom, 1000);
+
+
+
+
+function age(){
+    const n = Math.random();
+
+    var element = document.getElementById("age");
+    if (element){
+        element.textContent = n;
+    }
+}
+age();
+setInterval(age,1000);
+
