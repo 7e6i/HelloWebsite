6a503a74cd81adf8df6402e9181f8d5483391f3f
Spencer <spencera.rochester@gmail.com>
2024-04-26 15:21:38 -0400

    inefficient but it works

--- test.js
+++ test.js
@@ -3,11 +3,12 @@
 
 //https://stackoverflow.com/questions/69230383/failed-to-resolve-module-specifier-firebase-app
 
+
+// fancy firebase stuff
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
-import { getFirestore,doc, getDoc} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
+import { getFirestore,doc, getDoc, updateDoc} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 
-
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyC-mxNGZQQNFq89PYkLZ0TYlLT869nnYwQ",
     authDomain: "hellowebsite-f5cc0.firebaseapp.com",
     projectId: "hellowebsite-f5cc0",
@@ -15,30 +16,54 @@ import { getFirestore,doc, getDoc} from "https://www.gstatic.com/firebasejs/9.1.
     messagingSenderId: "471453856950",
     appId: "1:471453856950:web:2a3fb1deb5e59de5d9e217",
     measurementId: "G-WFVWVYQM1Z"
-  };
+};
 
-  // Initialize Firebase
+//init stuff
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
-
+// get counter doc
 const docRef = doc(db, "stuff", "counter");
-const docSnap = await getDoc(docRef);
 
-var stored_count;
-if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-    stored_count = docSnap.data()["count1"];
+
+// load from db
+async function load_from_db(){
+    const docSnap = await getDoc(docRef);
+
+
+    if (docSnap.exists()) {
+        var stored_count = docSnap.data()["count1"];
+        console.log(stored_count);
+    }
+    else {console.log("No such document!");}
+
+    document.getElementById("counter").textContent = stored_count;
+}
+
+load_from_db();
+
+
+
+async function addOne(){
+
+
+    const docSnap = await getDoc(docRef);
+    const plus = docSnap.data()['count1'] + 1;
+    await updateDoc(docRef, {"count1": plus});
+    counter.textContent = plus;
+
+    console.log(plus);
 
 }
-else {
-      // docSnap.data() will be undefined in this case
-      console.log("No such document!");
-}
 
-console.log(stored_count);
-var element = document.getElementById("counter");
-if (element){
-    element.textContent = stored_count;
-}
+document.getElementById("increase").addEventListener("click",addOne);
+
+
+
+
+
+
+
+
+
+
